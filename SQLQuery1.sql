5419fc3699ddaa60e15b53b67bab2c6adf16efb5
mayurpatil96k <mayur1792001@gmail.com>
2024-05-06 15:38:06 +0530

    [Mayur]Add: Adding new files for database join

---
+++ SQLQuery1.sql
@@ -0,0 +1,70 @@
+--Constraints
+--use contractpodAi;
+--Primary Key
+--create table mytable(id int primary key, name varchar(50));
+--exec sp_columns mytable;
+--alter table mytable drop CONSTRAINT PK__mytable__3213E83FD876FB32;
+
+--TURN ON PRIMARY KEY
+--ALTER INDEX PRIMARYKEY1 on MYTABLE
+--REBUILD;
+
+--FOREIIGN KEY
+-- Create Customers table
+--CREATE TABLE Customers (CustomerID INT PRIMARY KEY,Name VARCHAR(100),Email VARCHAR(100);
+
+-- Create Orders table with Foreign Key Constraint
+--CREATE TABLE Orders (OrderID INT PRIMARY KEY,CustomerID INT,OrderDate DATE, FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID);
+
+--JOINS
+CREATE TABLE Student (      
+  id int PRIMARY KEY IDENTITY,     
+  admission_no varchar(45) NOT NULL,  
+  first_name varchar(45) NOT NULL,      
+  last_name varchar(45) NOT NULL,  
+  age int,  
+  city varchar(25) NOT NULL      
+);    
+  
+CREATE TABLE Fee (   
+  admission_no varchar(45) NOT NULL,  
+  course varchar(45) NOT NULL,      
+  amount_paid int,    
+);  
+
+
+INSERT INTO Student (admission_no, first_name, last_name, age, city)       
+VALUES (3354,'Luisa', 'Evans', 13, 'Texas'),       
+(2135, 'Paul', 'Ward', 15, 'Alaska'),       
+(4321, 'Peter', 'Bennett', 14, 'California'),    
+(4213,'Carlos', 'Patterson', 17, 'New York'),       
+(5112, 'Rose', 'Huges', 16, 'Florida'),  
+(6113, 'Marielia', 'Simmons', 15, 'Arizona'),    
+(7555,'Antonio', 'Butler', 14, 'New York'),       
+(8345, 'Diego', 'Cox', 13, 'California');  
+  
+  
+INSERT INTO Fee (admission_no, course, amount_paid)       
+VALUES (3354,'Java', 20000),       
+(7555, 'Android', 22000),       
+(4321, 'Python', 18000),    
+(8345,'SQL', 15000),       
+(5112, 'Machine Learning', 30000); 
+
+--INNER JOIN
+SELECT Student.admission_no, Student.first_name, Student.last_name, Fee.course, Fee.amount_paid  
+FROM Student  
+INNER JOIN Fee  
+ON Student.admission_no = Fee.admission_no; 
+
+--SELF JOIN
+SELECT S1.first_name, S2.last_name, S2.city  
+FROM Student S1, Student S2  
+WHERE S1.id <> S2.iD AND S1.city = S2.city  
+ORDER BY S2.city;  
+
+--CROSS JOIN
+SELECT Student.admission_no, Student.first_name, Student.last_name, Fee.course, Fee.amount_paid  
+FROM Student  
+CROSS JOIN Fee  
+WHERE Student.admission_no = Fee.admission_no;  
---
+++ SQLQuery2.sql
@@ -0,0 +1,16 @@
+--DDL
+--Select clauses
+--select * from demo;
+--select name from demo;
+--select id,name from demo where id > 25;
+
+
+--Insert Clauses
+--insert into demo ("id","Name","Age")values(51,'Mayur',22);
+--insert into demo ("id","Name","Age")values(52,'Suyash',23),(53,'Omkar',23);
+
+--Update Clause
+--Update Demo set name='Rohit' where id=1;
+
+--Delete Clause
+--Delete from demo where id>50;
